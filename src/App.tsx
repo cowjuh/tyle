3f017a7f052efde58d9dcb8dd74746b6a5ea9d6a
Jenny Zhang <jenzhang530@gmail.com>
2022-01-26 11:51:35 -0800

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WelcomePage from "./components/pages/WelcomePage";
+import PairingPage from "./components/pages/PairingPage";
+import NotFound from "./components/pages/NotFound";
+import DataMode from "./components/pages/Playground/DataMode";
+import DrawingMode from "./components/pages/Playground/DrawingMode";
+import ProgramMode from "./components/pages/Playground/ProgramMode";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<WelcomePage />}></Route>
+          <Route path="/pairing" element={<PairingPage />}></Route>
+          <Route path="/playground" element={<DataMode />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
