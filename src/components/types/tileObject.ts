fa9f8720f7d93362501c4d3f59c6e6ecebb2dea8
Jenny Zhang <jenzhang530@gmail.com>
2022-01-26 15:40:04 -0800

    Update LED hex codes

--- src/components/types/tileObject.ts
+++ src/components/types/tileObject.ts
@@ -9,4 +9,16 @@ export interface SingleLEDPattern {
 }
 
 export type LEDRow = Array<SingleLEDPattern>;
-export type LEDColours = "red" | "orange" | "yellow" | "green" | "cyan" | "blue" | "purple" | "pink" | "white" | "none"
+export type LEDColours = Colors
+export enum Colors {
+    red = "#FF5B5B",
+    orange = "#FF9820",
+    yellow = "#FFDB7D",
+    green = "#72ED7E",
+    cyan = "#82DCD7",
+    blue = "#3443C7",
+    purple = "#807EE7",
+    pink = "#E767DA",
+    white = "#FFFFFF",
+    none = "",
+}
--- src/mockData/mockTileObject.ts
+++ src/mockData/mockTileObject.ts
@@ -1,22 +1,22 @@
-import { TileObject } from "../components/types/tileObject";
+import { Colors, TileObject } from "../components/types/tileObject";
 
 export const mockTileObject: Array<TileObject> = [
     {
         tileId: 'tileA',
         ledConfig: [
-            [{color: "red", opacity: 100}, {color: "red", opacity: 100}, {color: "red", opacity: 100}, {color: "red", opacity: 100}, ],
-            [{color: "cyan", opacity: 100}, {color: "cyan", opacity: 100}, {color: "cyan", opacity: 100}, {color: "cyan", opacity: 100}, ],
-            [{color: "pink", opacity: 100}, {color: "pink", opacity: 100}, {color: "pink", opacity: 100}, {color: "pink", opacity: 100}, ],
-            [{color: "green", opacity: 100}, {color: "green", opacity: 100}, {color: "green", opacity: 100}, {color: "green", opacity: 100}, ],
+            [{color: Colors.red, opacity: 100}, {color: Colors.red, opacity: 100}, {color: Colors.red, opacity: 100}, {color: Colors.red, opacity: 100}, ],
+            [{color: Colors.cyan, opacity: 100}, {color: Colors.cyan, opacity: 100}, {color: Colors.cyan, opacity: 100}, {color: Colors.cyan, opacity: 100}, ],
+            [{color: Colors.pink, opacity: 100}, {color: Colors.pink, opacity: 100}, {color: Colors.pink, opacity: 100}, {color: Colors.pink, opacity: 100}, ],
+            [{color: Colors.green, opacity: 100}, {color: Colors.green, opacity: 100}, {color: Colors.green, opacity: 100}, {color: Colors.green, opacity: 100}, ],
         ]
     },
     {
         tileId: 'tileB',
         ledConfig: [
-            [{color: "cyan", opacity: 100}, {color: "cyan", opacity: 100}, {color: "cyan", opacity: 100}, {color: "cyan", opacity: 100}, ],
-            [{color: "green", opacity: 100}, {color: "green", opacity: 100}, {color: "green", opacity: 100}, {color: "green", opacity: 100}, ],
-            [{color: "red", opacity: 100}, {color: "red", opacity: 100}, {color: "red", opacity: 100}, {color: "red", opacity: 100}, ],
-            [{color: "pink", opacity: 100}, {color: "pink", opacity: 100}, {color: "pink", opacity: 100}, {color: "pink", opacity: 100}, ],
+            [{color: Colors.cyan, opacity: 100}, {color: Colors.cyan, opacity: 100}, {color: Colors.cyan, opacity: 100}, {color: Colors.cyan, opacity: 100}, ],
+            [{color: Colors.green, opacity: 100}, {color: Colors.green, opacity: 100}, {color: Colors.green, opacity: 100}, {color: Colors.green, opacity: 100}, ],
+            [{color: Colors.purple, opacity: 100}, {color: Colors.purple, opacity: 100}, {color: Colors.purple, opacity: 100}, {color: Colors.purple, opacity: 100}, ],
+            [{color: Colors.pink, opacity: 100}, {color: Colors.pink, opacity: 100}, {color: Colors.pink, opacity: 100}, {color: Colors.pink, opacity: 100}, ],
         ]
     }
 ]
