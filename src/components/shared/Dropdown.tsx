966ba8c6958cca1a0b743c223b48e53abaa4c698
Jenny Zhang <jenzhang530@gmail.com>
2022-03-02 15:53:23 -0800

    Style and configure dropdown component

--- src/components/shared/Dropdown.tsx
+++ src/components/shared/Dropdown.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { StateOperator } from "../types/types";
+
 interface DropdownProps {
   options: Array<string>;
+  onChange: (val: string) => void;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ options }) => {
+const DropdownStyled = styled.select`
+  background: #ffffff20;
+  color: white;
+  padding: 5px;
+  width: 100%;
+  border-radius: 5px;
+`;
+
+const Dropdown: React.FC<DropdownProps> = ({ options, onChange }) => {
+  const [value, setValue] = useState<string>(StateOperator.greaterThan);
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+    setValue(e.currentTarget.value);
+    onChange(e.currentTarget.value);
+  };
   return (
     <form>
-      <select>
+      <DropdownStyled value={value} onChange={handleChange}>
         {options.map((option) => {
           return <option>{option}</option>;
         })}
-      </select>
+      </DropdownStyled>
     </form>
   );
 };
