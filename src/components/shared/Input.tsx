f4e7e990a43f936c89f4e313aa309cc4de6f6e92
Jenny Zhang <jenzhang530@gmail.com>
2022-03-02 15:53:34 -0800

    Style and configure input element

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { MAX_PRESSURE_SENSOR_VALUE } from "../../utils/constants";
 
@@ -7,14 +8,29 @@ const InputContainer = styled.input`
   border-radius: 3px;
   padding: 8px;
   color: white;
+  max-width: 100%;
+  flex: 1;
 `;
 
-const NumericalInput = () => {
+interface NumericalInputProps {
+  onChange: (value: number) => void;
+}
+
+const NumericalInput: React.FC<NumericalInputProps> = ({ onChange }) => {
+  const [value, setValue] = useState<string>();
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setValue(e.currentTarget.value);
+    onChange(Number(e.currentTarget.value));
+  };
   return (
     <InputContainer
       type={"number"}
       min={0}
+      defaultValue={0}
       max={MAX_PRESSURE_SENSOR_VALUE}
+      value={value}
+      onChange={handleChange}
     ></InputContainer>
   );
 };
