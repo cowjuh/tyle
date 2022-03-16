e7a8b1ab4d50ee73c993815afc32f298dfd3ee11
Jenny Zhang <jenzhang530@gmail.com>
2022-03-15 22:05:41 -0700

    Rename StateCreator to StateEditor

--- src/components/shared/Sidebar/programMode/StateEditor.tsx
+++ src/components/shared/Sidebar/programMode/StateEditor.tsx
@@ -1,66 +1,155 @@
 import { useMemo, useState } from "react";
-import { generateStateId } from "../../../../utils/helpers";
+import styled from "styled-components";
 import { Color, StateOperator } from "../../../types/types";
 import Button from "../../Atoms/Button";
 import ColorPicker from "../../ColorPicker/ColorPicker";
+import Dropdown from "../../Atoms/Dropdown";
+import NumericalInput from "../../Atoms/Input";
+import { TextButton } from "../../Atoms/TextButton";
+import {
+  SidebarHorizContainer,
+  SidebarInnerContainer,
+} from "../../../Containers";
+import { useLocation, useNavigate } from "react-router-dom";
+import {
+  BASE_ROUTE_PROGRAM_MODE,
+  DROPDOWN_OPTIONS,
+} from "../../../../utils/constants";
+import { getStateId } from "../../../../utils/helpers";
+import { useProgramModeContext } from "../../../hooks/useProgramModeContext";
 
-const dropdownOptions = [
-  StateOperator.greaterThan,
-  StateOperator.lessThan,
-  StateOperator.equalTo,
-  StateOperator.notEqualTo,
-  StateOperator.between,
-];
+const BetweenOperatorInputContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-interface StateProps {
-  id?: string;
-  onSave: (
-    id: string,
-    color: Color,
-    operator: StateOperator,
-    input1: number,
-    input2?: number | undefined
-  ) => void;
-}
+const BetweenOperatorSupportText = styled.div`
+  padding: 0 8px;
+`;
 
-const StateEditor: React.FC<StateProps> = ({ onSave, id }) => {
-  const [operator, setOperator] = useState<StateOperator>(
-    StateOperator.greaterThan
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const StateCreator = () => {
+  const location = useLocation();
+  const stateId = getStateId(location.pathname); // undefined if no existing stateId exists
+  const navigate = useNavigate();
+
+  const {
+    updateProgramModeStates,
+    createProgramModeState,
+    getStateObjectById,
+    deleteStateObjectById,
+  } = useProgramModeContext();
+
+  const stateObject = stateId ? getStateObjectById(stateId) : undefined;
+  const [selectedColor, setSelectedColor] = useState<Color>(
+    (stateObject && stateObject?.color) || Color.none
   );
-  const [selectedColor, setSelectedColor] = useState<Color>(Color.none);
-  const [input1, setInput1] = useState<number>(0);
-  const [input2, setInput2] = useState<number>(0);
-  const singleInputOperator = useMemo(
-    () => operator != StateOperator.between,
+  const [input1, setInput1] = useState<number>(
+    (stateObject && stateObject?.primaryInputValue) || 0
+  );
+  const [input2, setInput2] = useState<number>(
+    (stateObject && stateObject?.secondaryInputValue) || 0
+  );
+  const [operator, setOperator] = useState<StateOperator>(
+    (stateObject && stateObject?.operator) || StateOperator.greaterThan
+  );
+
+  const isSingleInputOperator = useMemo(
+    () => operator !== StateOperator.between,
     [operator]
   );
+
+  const isCreatingNewState = useMemo(() => {
+    return stateId === undefined;
+  }, [stateId]);
 
   const onSetColor = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     color: Color
   ) => {
-    console.log(color);
+    e.preventDefault();
     setSelectedColor(color);
   };
 
-  // TODO: This currently uses the date as an ID which is NON IDEAL!!
+  const handleOnCreate = () => {
+    createProgramModeState(selectedColor, operator, input1, input2);
+    navigate(BASE_ROUTE_PROGRAM_MODE);
+  };
+
   const handleOnSave = () => {
-    onSave(id || generateStateId(), selectedColor, operator, input1, input2);
+    if (stateId) {
+      updateProgramModeStates(stateId, selectedColor, operator, input1, input2);
+      navigate(BASE_ROUTE_PROGRAM_MODE);
+    }
+  };
+
+  const handleOnDelete = () => {
+    stateId && deleteStateObjectById(stateId);
+    navigate(BASE_ROUTE_PROGRAM_MODE);
+  };
+
+  const onDropdownChange = (operator: string) => {
+    setOperator(StateOperator[operator as keyof typeof StateOperator]);
+  };
+
+  const onInput1Change = (val: number) => setInput1(val);
+
+  const onInput2Change = (val: number) => setInput2(val);
+
+  const onBack = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    navigate(BASE_ROUTE_PROGRAM_MODE);
   };
 
   return (
     <>
-      <h4>When tile force is</h4>
-      {/* <Dropdown options={dropdownOptions} /> */}
-      {/* {singleInputOperator && <NumericalInput />}
-      {!singleInputOperator && <NumericalInput />} */}
-      <h4>Set color to</h4>
-      <ColorPicker onSetColor={onSetColor} />
-      <Button onClick={handleOnSave}>
-        {id ? "Save state" : "Create state"}
-      </Button>
+      <SidebarHorizContainer>
+        <TextButton onClick={onBack}>{`← Back`}</TextButton>
+      </SidebarHorizContainer>
+      <SidebarInnerContainer>
+        <h4>When tile force is</h4>
+        <Dropdown
+          options={DROPDOWN_OPTIONS}
+          onChange={onDropdownChange}
+          initialValue={operator}
+        />
+        {isSingleInputOperator && (
+          <NumericalInput onChange={onInput1Change} initialValue={input1} />
+        )}
+        {!isSingleInputOperator && (
+          <BetweenOperatorInputContainer>
+            <NumericalInput onChange={onInput1Change} initialValue={input1} />
+            <BetweenOperatorSupportText>and</BetweenOperatorSupportText>
+            <NumericalInput onChange={onInput2Change} initialValue={input2} />
+          </BetweenOperatorInputContainer>
+        )}
+        <h4>Set color to</h4>
+        <ColorPicker onSetColor={onSetColor} />
+        <ButtonContainer>
+          <Button onClick={isCreatingNewState ? handleOnCreate : handleOnSave}>
+            {isCreatingNewState ? "Create state" : "Save State"}
+          </Button>
+          <TextButton onClick={onBack}>Cancel</TextButton>
+          <TextButton onClick={handleOnDelete}>Delete</TextButton>
+        </ButtonContainer>
+      </SidebarInnerContainer>
     </>
   );
 };
 
-export default StateEditor;
+export default StateCreator;
+function createProgramModeState(
+  color: Color,
+  operator: StateOperator,
+  input1: number,
+  input2: number | undefined
+) {
+  throw new Error("Function not implemented.");
+}
