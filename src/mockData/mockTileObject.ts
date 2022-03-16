9a3f5bc64f45235a15e6e7750f9a4b7830205dc6
Jenny Zhang <jenzhang530@gmail.com>
2022-03-16 14:54:00 -0700

    Create empty mock tile object

--- src/mockData/mockTileObject.ts
+++ src/mockData/mockTileObject.ts
@@ -213,3 +213,217 @@ export const mockDrawModeTileGrid: TileGridObject = [
     },
   ],
 ];
+
+export const mockProgramModeTileGrid: TileGridObject = [
+  [
+    {
+      tileId: "tileA",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+      ],
+    },
+    {
+      tileId: "tileB",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+        [
+          { color: Color.purple, opacity: 100 },
+          { color: Color.purple, opacity: 100 },
+          { color: Color.purple, opacity: 100 },
+          { color: Color.purple, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+      ],
+    },
+    {
+      tileId: "tileC",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+      ],
+    },
+    {
+      tileId: "tileD",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+        [
+          { color: Color.purple, opacity: 100 },
+          { color: Color.purple, opacity: 100 },
+          { color: Color.purple, opacity: 100 },
+          { color: Color.purple, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+      ],
+    },
+  ],
+  [
+    {
+      tileId: "tileE",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+      ],
+    },
+    {
+      tileId: "tileF",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+      ],
+    },
+    {
+      tileId: "tileG",
+      ledConfig: [
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+          { color: Color.none, opacity: 100 },
+        ],
+        [
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+          { color: Color.pink, opacity: 100 },
+        ],
+        [
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+          { color: Color.green, opacity: 100 },
+        ],
+      ],
+    },
+    {
+      tileId: "empty",
+      ledConfig: [],
+    },
+  ],
+];
