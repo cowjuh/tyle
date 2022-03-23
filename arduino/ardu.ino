f0f0178f27c4d34d77a3d380b74aa6c0693b8904
Jenny Zhang <jenzhang530@gmail.com>
2022-03-23 01:31:22 -0700

    Add documentation to arduino sketch

--- arduino/ardu.ino
+++ arduino/ardu.ino
@@ -1,3 +1,23 @@
+/*
+The data that gets stored in incomingString looks like this:
+(255,152,32,1),(255,152,32,1), ... (231,103,218,1)
+
+corresponding to:
+(R,G,B,A)
+
+Each set of rgb values is contained in parentheses, and comma-separated.
+There are 16 RGBA values in total.
+
+The rgba values are listed according to this indexing
+[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+Which is represented like this physically
+[0,1,2,3]
+[4,5,6,7],
+[8,9,10,11],
+[12,13,14,15]
+*/
+
 String incomingString = "";
 
 void setup()
