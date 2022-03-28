5057bb56521abc8ece30b5c063ff949a5e143b2e
Jenny Zhang <jenzhang530@gmail.com>
2022-03-28 02:21:07 -0700

    Create nodeJS websocket server

--- server/server.js
+++ server/server.js
@@ -1,44 +1,34 @@
-const { SerialPort } = require("serialport");
-const { ReadlineParser } = require("@serialport/parser-readline");
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const bodyParser = require("body-parser");
-const { arduinoPort, parser, writeToSerial } = require("./arduinoSerial");
+// Importing the required modules
+const WebSocketServer = require("ws");
 
-// ROUTE IMPORTS
-const programModeRouter = require("./routes/programMode");
-const dataModeRouter = require("./routes/dataMode");
-const drawModeRouter = require("./routes/drawMode");
+// Creating a new websocket server
+var wss = new WebSocketServer.Server({ port: 3001 }),
+  CLIENTS = [];
 
-// CONSTANT VARIABLES
-const SERVER_PORT = 5001;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-
-// API ROUTES
-app.use("/api/programmode", programModeRouter);
-app.use("/api/datamode", dataModeRouter);
-app.use("/api/drawmode", drawModeRouter);
-
-app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`));
-
-// ARDUINO PORT AND PARSER SETUP
-
-arduinoPort.on("error", function (err) {
-  console.log("Error: ", err.message);
-});
-
-async function init() {
-  await sleep(1500);
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
+// Creating connection using websocket
+wss.on("connection", (ws) => {
+  CLIENTS.push(ws);
+  ws.on("message", function (message) {
+    sendAll(message);
   });
-}
+  ws.send("NEW USER JOINED");
+  // sending message
+  ws.on("message", (data) => {
+    console.log("Received Local Storage Data");
+  });
+  // handling what to do when clients disconnects from server
+  ws.on("close", () => {
+    console.log("CLIENT DISCONNECTED");
+  });
+  // handling client connection error
+  ws.onerror = function () {
+    console.log("Some Error occurred");
+  };
+});
+console.log("The WebSocket server is running on port 3001");
 
-init();
+function sendAll(message) {
+  for (var i = 0; i < CLIENTS.length; i++) {
+    CLIENTS[i].send("Message: " + message);
+  }
+}
