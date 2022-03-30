7cf6105c2ee9afea47e541a2521118f7295838f8
Jenny Zhang <jenzhang530@gmail.com>
2022-03-30 14:41:20 -0700

    Abstract socket initialization to external file

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,14 +1,32 @@
-import { io } from "socket.io-client";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import {
+  TileGridObject,
+  WSMessageObject,
+  WSMessageType,
+} from "../components/types/types";
 
-export const espSocket = io("/");
+const HOST = "192.168.0.41";
+const IPHONE_HOTSPOT = "172.20.10.2";
+const UBC_SECURE = "128.189.131.17";
+const PORT = 3001;
 
-export const socketSendMessage = (msg: string) => {
-  console.log("Trying to send message here");
-  espSocket.emit("send_message", { msg });
+export const wsClient = new W3CWebSocket(`ws://${UBC_SECURE}:${PORT}`);
+
+const constructWSObject = (
+  type: WSMessageType,
+  data: Object
+): WSMessageObject => {
+  return { type: type, data: data };
 };
 
-export const socketReceiveMesage = () => {
-  espSocket.on("receive_message", (data) => {
-    console.log(data.message);
-  });
+export const syncTileGrid = () => {
+  const messageObj: WSMessageObject = {
+    type: WSMessageType.sync_grid,
+    data: {},
+  };
+  wsClient.send(JSON.stringify(messageObj));
+};
+
+export const emitLEDPattern = (tileGridObj: TileGridObject) => {
+  wsClient.send(constructWSObject(WSMessageType.led_pattern, tileGridObj));
 };
