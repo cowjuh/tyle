325989dad5920d5116ae9f15c785a14eb48237ce
Jenny Zhang <jenzhang530@gmail.com>
2022-04-01 15:45:33 -0700

    ESP32 Fixes @ IGEN Clubroom

--- arduino/Tyle_Esp32/Tyle_Esp32.ino
+++ arduino/Tyle_Esp32/Tyle_Esp32.ino
@@ -6,22 +6,22 @@
 
 #define MAX_TILES 5
 
-const char *ssid = "TestAP";                                 // Enter YOUR SSID
-const char *password = "gvww7028";                           // Enter YOUR Password
-const char *websockets_server_host = "ws://192.168.43.230:"; // Enter ws://YOUR-SERVER-ADDRESS
-const uint16_t websockets_server_port = 3001;                // DO NOT CHANGE
+const char* ssid = "TestAP";                             // Enter YOUR SSID
+const char* password = "gvww7028";                        // Enter YOUR Password
+const char* websockets_server_host = "ws://192.168.43.230:"; // Enter ws://YOUR-SERVER-ADDRESS
+const uint16_t websockets_server_port = 3001;              // DO NOT CHANGE
 
 using namespace websockets;
 WebsocketsClient client;
 
-int numTiles = 1;
+int numTiles = 3;
 String tileNeighbours[MAX_TILES] = {};
 byte tileIDs[MAX_TILES] = {};
 
 int cellValues[MAX_TILES][4] = {};
 
 unsigned long lastPoll = 0;
-const int pollDelay = 500; // ms
+const int pollDelay = 500; //ms
 
 void setup()
 {
@@ -30,8 +30,7 @@ void setup()
   wifiSetup();
 }
 
-void wifiSetup()
-{
+void wifiSetup() {
   // Connect to wifi
   WiFi.begin(ssid, password);
 
@@ -91,24 +90,18 @@ void onEventsCallback(WebsocketsEvent event, String data)
 void onMessageCallback(WebsocketsMessage message)
 {
   String msg = message.data();
-  if (msg.charAt(0) == 'C')
-  { // update server with tile config
+  if (msg.charAt(0) == 'C') { //update server with tile config
     getNeighbours();
     client.send(buildConfigString());
-  }
-  else if (msg.charAt(0) == 'D' || msg.charAt(0) == 'F')
-  {
-    for (int j = 0; j < msg.length(); j++)
-    {
-      if (msg.charAt(j) == 'D')
-      {
+  } else if (msg.charAt(0) == 'D' || msg.charAt(0) == 'F') {
+    for (int j = 0; j < msg.length(); j++) {
+      if (msg.charAt(j) == 'D') {
         j += 2;
-        int id = msg.charAt(j) - '0'; // fails if id > 9 TODO
+        int id = msg.charAt(j) - '0'; //fails if id > 9 TODO
         j += 2;
         String numString = "";
         int k = j;
-        while (msg.charAt(k) != ' ')
-        {
+        while (msg.charAt(k) != ' ') {
           numString += msg.charAt(k);
           k++;
         }
@@ -116,106 +109,95 @@ void onMessageCallback(WebsocketsMessage message)
         j = ++k;
         byte updateArray[numDiff * 4] = {};
         int updateIndex = 0;
-        while (j < msg.length() && (msg.charAt(0) != 'D' || msg.charAt(0) != 'F'))
-        {
+        while (j < msg.length() && (msg.charAt(0) != 'D' || msg.charAt(0) != 'F')) {
           int next = msg.substring(j, j + 3).toInt();
           updateArray[updateIndex] = lowByte(next);
           updateIndex++;
           j += 4;
         }
-        updateLEDs(id, updateArray, numDiff * 4);
-      }
-      else
-      {
+        updateLEDs(id, updateArray, numDiff * 4); //here
+      } else {
         j += 2;
-        int id = msg.charAt(j) - '0'; // fails if id > 9 TODO
+        int id = msg.charAt(j) - '0'; //fails if id > 9 TODO
         j += 2;
         byte updateArray[48] = {};
         int pos = 0;
         int c = 0;
-        while (pos < 48)
-        {
+        while (pos < 48) {
           int next = msg.substring(c, c + 3).toInt();
           updateArray[pos] = lowByte(next);
           pos++;
           c += 4;
         }
-        updateLEDs(id, updateArray, 48);
+        updateLEDs(id, updateArray, 48); //here
       }
     }
-  }
-  else
-  {
+  } else {
     Serial.print("Malformed message recieved");
   }
 }
 
-String buildConfigString()
-{
+String buildConfigString() {
   String configString = "";
-  for (int i = 0; i < numTiles; i++)
-  {
+  for (int i = 0; i < numTiles; i++) {
     configString += tileNeighbours[i];
   }
   return configString;
 }
 
-void updateLEDs(int id, byte updateArray[], int len)
-{
+
+
+void updateLEDs(int id, byte updateArray[], int len) {
   Wire.beginTransmission(tileIDs[id]);
-  for (int i = 0; i < len; i++)
-  {
+  for (int i = 0; i < len; i++) {
     Wire.write(updateArray[i]);
   }
   Wire.endTransmission();
+
 }
 
-void getSensors(int id)
-{
+void getSensors(int id) {
   int count = 0;
 
   Wire.requestFrom(tileIDs[id], 8);
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     cellValues[id][i] = (Wire.read() << 8) | Wire.read();
   }
 }
 
-void getNeighbours()
-{
-
-  for (int i = 0; i < numTiles; i++)
-  {
+void getNeighbours() {
+  
+  for (int i = 0; i < numTiles; i++){
     Wire.requestFrom(tileIDs[i], 4);
     String s = "";
-    while (Wire.available())
-    {
+    while (Wire.available()){
       s += Wire.read();
     }
     tileNeighbours[i] = s;
   }
 }
 
-void pollTiles()
-{
+
+void pollTiles() {
   lastPoll = millis();
-  for (int i = 0; i < numTiles; i++)
-  {
+  for (int i = 0; i < numTiles; i++) {
     getSensors(i);
   }
 }
 
-String buildSensorString()
-{
+String buildSensorString() {
   String sensorString = "";
-  for (int i = 0; i < numTiles; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      sensorString += cellValues[i][j] + " ";
+  for (int i = 0; i < numTiles; i++) {
+    for (int j = 0; j < 4; j++) {
+      sensorString += String(cellValues[i][j]) + " ";
+      Serial.print(String(cellValues[i][j]) + " ");
+      
     }
+    Serial.println();
   }
   sensorString.trim();
+  Serial.println(sensorString);
+  return sensorString;
 }
 
 void loop()
@@ -229,10 +211,10 @@ void loop()
     @jonah this is where you'll want to send tile sensor
     values at every interval (that you want)
   */
-  if (lastPoll < millis() + pollDelay)
-  {
+  if (lastPoll < millis() + pollDelay) {
     pollTiles();
     client.send(buildSensorString());
   }
-  delay(10);
+  delay(50);
+  
 }
