80a884683c7d121332432d9079ec6c13687cfdb2
Jenny Zhang <jenzhang530@gmail.com>
2022-04-04 22:52:09 -0700

    Delete unused mock object

--- src/components/context/programModeContext.tsx
+++ src/components/context/programModeContext.tsx
@@ -1,11 +1,5 @@
 import { createContext } from "react";
-import {
-  Color,
-  ProgramModeStateObject,
-  ProgramModeStatesObject,
-  StateOperator,
-  TileGridObject,
-} from "../types/types";
+import { ProgramModeStatesObject, TileGridObject } from "../types/types";
 
 interface ProgramModeContextProps {
   programModeStates: ProgramModeStatesObject;
@@ -15,17 +9,8 @@ interface ProgramModeContextProps {
   setTempTileGridObject: (tileGridObject: TileGridObject) => void;
 }
 
-const mockProgramModeState: ProgramModeStateObject = {
-  color: Color.blue,
-  id: "1",
-  operator: StateOperator.greaterThan,
-  primaryInputValue: 200,
-  selectedLEDs: ["hello"],
-  tileId: { letter: "A", num: 0 },
-};
-
 export const ProgramModeContext = createContext<ProgramModeContextProps>({
-  programModeStates: [mockProgramModeState],
+  programModeStates: [],
   setProgramModeStates: () => {},
   tempTileGridObject: [],
   setTempTileGridObject: () => {},
