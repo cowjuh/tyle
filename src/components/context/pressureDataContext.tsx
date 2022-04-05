7fcc2d5d5fc6f9604ac50635393b2a77455a5a06
Jenny Zhang <jenzhang530@gmail.com>
2022-04-04 19:14:21 -0700

    Implement PressureDataContext

--- src/components/context/pressureDataContext.tsx
+++ src/components/context/pressureDataContext.tsx
@@ -1,4 +1,32 @@
-import { createContext } from "react";
+import { createContext, ReactChild, useEffect, useState } from "react";
 import { TileGridPressure } from "../types/types";
 
-export const PressureDataContext = createContext<TileGridPressure>([]);
+interface IPressureDataContext {
+  pressureDataObject: TileGridPressure;
+  setPressureDataObject: (tileGridPressure: TileGridPressure) => void;
+}
+
+export const PressureDataContext = createContext<IPressureDataContext>({
+  pressureDataObject: [],
+  setPressureDataObject: () => {},
+});
+
+interface IPressureDataProvider {
+  children: ReactChild;
+}
+
+export const PressureDataProvider = (props: IPressureDataProvider) => {
+  const [pressureDataObject, setPressureDataObject] =
+    useState<TileGridPressure>([{ tileId: 1, values: [1, 2, 3, 4] }]);
+
+  const pressureDataContextValue = {
+    pressureDataObject,
+    setPressureDataObject,
+  };
+
+  return (
+    <PressureDataContext.Provider value={pressureDataContextValue}>
+      {props.children}
+    </PressureDataContext.Provider>
+  );
+};
