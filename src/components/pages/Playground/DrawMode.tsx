aae03ae30fd65fdd7bc3ebe0523bd90ba4570d92
Jenny Zhang <jenzhang530@gmail.com>
2022-04-04 22:53:58 -0700

    Abstract context provider logic and remove imports

--- src/components/pages/Playground/DrawMode.tsx
+++ src/components/pages/Playground/DrawMode.tsx
@@ -1,33 +1,18 @@
-import { useContext, useState } from "react";
-import { getDrawModeTileGridObject } from "../../../utils/helpers";
 import {
   FullWidthHeightCenteredContainer,
   PlayGroundParentContainer,
 } from "../../Containers";
-import { DrawModeContext } from "../../context/drawModeContext";
-import { GlobalContext } from "../../context/globalContext";
 import Sidebar from "../../shared/Sidebar/Sidebar";
-import { TileGridObject } from "../../types/types";
 import TileCanvas from "./DrawModeTileCanvas";
 
 const DrawingMode = () => {
-  const { globalTileGridObject } = useContext(GlobalContext);
-  const [drawModeTileGridObject, setDrawModeTileGridObject] =
-    useState<TileGridObject>(getDrawModeTileGridObject || globalTileGridObject);
-  const tileGridContextValue = {
-    drawModeTileGridObject,
-    setDrawModeTileGridObject,
-  };
-
   return (
-    <DrawModeContext.Provider value={tileGridContextValue}>
-      <FullWidthHeightCenteredContainer>
-        <PlayGroundParentContainer>
-          <Sidebar />
-          <TileCanvas />
-        </PlayGroundParentContainer>
-      </FullWidthHeightCenteredContainer>
-    </DrawModeContext.Provider>
+    <FullWidthHeightCenteredContainer>
+      <PlayGroundParentContainer>
+        <Sidebar />
+        <TileCanvas />
+      </PlayGroundParentContainer>
+    </FullWidthHeightCenteredContainer>
   );
 };
 
