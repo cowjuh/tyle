7aad79a64f3bbb2328a5ce17afa28c798a8e9d36
Jenny Zhang <jenzhang530@gmail.com>
2022-04-05 00:05:34 -0700

    Abstract handling JSON vs Raw Text to separate functions

--- server/server.ts
+++ server/server.ts
@@ -14,9 +14,14 @@ const CYAN = "\x1b[36m%s\x1b[0m";
 const GREEN = "\x1b[35m";
 const YELLOW = "\x1b[33m";
 
+// CONSOLE.LOG Divider
+const DIVIDER = "\n\n--------------------------------------------------------";
+
 // Creating a new websocket server
 var wss = new WebSocketServer.Server({ port: PORT });
 var CLIENTS: WebSocketServer.WebSocket[] = [];
+
+// ! FOR EMULATION ONLY
 var tileShapeStr1 = "m02013000002";
 var tileShapeStr2 = "m02010302000";
 var shapeFlag = true;
@@ -27,8 +32,26 @@ wss.on("connection", (ws) => {
   CLIENTS.push(ws);
   broadcast(WSMessageType.new_client, "NEW USER JOINED", ws);
 
+  ws.on("message", (data) => {
+    var messageStr = data.toString();
+    console.log(DIVIDER);
+    console.log(YELLOW, "[SERVER] Received: ", messageStr);
+
+    isJson(messageStr)
+      ? handleJSONMessage(ws, messageStr)
+      : handleRawTextMessage(ws, messageStr);
+  });
+
+  ws.on("close", () => {
+    console.log("CLIENT DISCONNECTED");
+  });
+
+  ws.onerror = function () {
+    console.log("Some Error occurred");
+  };
+
   // EMITS EVERY SECOND TO ALL CLIENTS
-  // FOR TESTING ONLY, THIS BEHAVIOUR SHOULD BE DONE BY THE ESP32 ITSELF
+  // ! FOR TESTING ONLY, THIS BEHAVIOUR SHOULD BE DONE BY THE ESP32 ITSELF
   setInterval(() => {
     sendWSObject(
       WSMessageType.pressure_data,
@@ -38,87 +61,78 @@ wss.on("connection", (ws) => {
       ws
     );
   }, 1000);
-
-  // ON RECEPTION OF MESSAGES
-  ws.on("message", (data) => {
-    let tileShapeStr = shapeFlag ? tileShapeStr1 : tileShapeStr2;
-    var messageStr = data.toString();
-    console.log(
-      "\n\n-------------------------------------------------------------------"
-    );
-    console.log(YELLOW, "[SERVER] Received: ", messageStr);
-
-    /**
-     * JSON MESSAGES: From FRONTEND/WEB
-     */
-    if (isJson(messageStr)) {
-      var messageObj: WSMessageObject = JSON.parse(messageStr);
-      switch (messageObj.type) {
-        // EMISSION OF LED PATTERN
-        case WSMessageType.led_pattern:
-          console.log(CYAN, "[UI] LED PATTERN EMITTED");
-          broadcast(
-            WSMessageType.led_pattern,
-            messageObj.data.toString(),
-            ws,
-            true
-          );
-          break;
-
-        /**
-         * UI Making a request to sync the tile grid
-         * The UI will send a single character, 'C' to the ESP32
-         */
-        case WSMessageType.request_sync_grid:
-          console.log(CYAN, "[UI] REQUEST SYNC TILE GRID");
-          broadcast(WSMessageType.request_sync_grid, "C", ws, true);
-
-          // DELETE BELOW WHEN FULLY IMPLEMENTED
-          var messageObjJSONStr: string = constructWSObject(
-            WSMessageType.request_sync_grid,
-            parseTileGridShape(tileShapeStr)
-          );
-          ws.send(messageObjJSONStr);
-          shapeFlag = !shapeFlag;
-          break;
-
-        default:
-          break;
-      }
-
-      /**
-       * NON-JSON/RAW TEXT MESSAGES: FROM ESP32
-       */
-    } else {
-      if (messageStr.charAt(0) === "m") {
-        console.log(GREEN, "[ESP32] SENT SYNC TILE GRID");
-        var messageObjJSONStr: string = constructWSObject(
-          WSMessageType.request_sync_grid,
-          parseTileGridShape(messageStr)
-        );
-      } else {
-        console.log(GREEN, "[ESP32] PRESSURE DATA EMITTED");
-        sendWSObject(
-          WSMessageType.pressure_data,
-          parseESP32PressureData(messageStr),
-          ws
-        );
-      }
-    }
-  });
-
-  // handling what to do when clients disconnects from server
-  ws.on("close", () => {
-    console.log("CLIENT DISCONNECTED");
-  });
-  // handling client connection error
-  ws.onerror = function () {
-    console.log("Some Error occurred");
-  };
 });
 
 console.log(`The WebSocket server is running on port ${PORT}`);
 
+/**
+ * Handles JSON formatted messages to the WS Server
+ * This is assumed to be from web clients
+ * @param ws
+ * @param message
+ */
+function handleJSONMessage(ws: WebSocketServer.WebSocket, message: string) {
+  let tileShapeStr = shapeFlag ? tileShapeStr1 : tileShapeStr2;
+  var messageObj: WSMessageObject = JSON.parse(message);
+  switch (messageObj.type) {
+    // EMISSION OF LED PATTERN
+    case WSMessageType.led_pattern:
+      console.log(CYAN, "[UI] LED PATTERN EMITTED");
+      broadcast(
+        WSMessageType.led_pattern,
+        messageObj.data.toString(),
+        ws,
+        true
+      );
+      break;
+
+    // UI Making a request to sync the tile grid
+    // The UI will send a single character, 'C' to the ESP32
+    case WSMessageType.request_sync_grid:
+      console.log(CYAN, "[UI] REQUEST SYNC TILE GRID");
+      broadcast(WSMessageType.request_sync_grid, "C", ws, true);
+
+      // ! DELETE BELOW WHEN FULLY IMPLEMENTED
+      var messageObjJSONStr: string = constructWSObject(
+        WSMessageType.request_sync_grid,
+        parseTileGridShape(tileShapeStr)
+      );
+      ws.send(messageObjJSONStr);
+      shapeFlag = !shapeFlag;
+      break;
+
+    default:
+      break;
+  }
+}
+
+/**
+ * Handles raw text messages to the WS server.
+ * These are assumed to come from the ESP32.
+ * @param ws
+ * @param message
+ */
+function handleRawTextMessage(ws: WebSocketServer.WebSocket, message: string) {
+  if (message.charAt(0) === "m") {
+    console.log(GREEN, "[ESP32] SENT SYNC TILE GRID");
+    var messageObjJSONStr: string = constructWSObject(
+      WSMessageType.request_sync_grid,
+      parseTileGridShape(message)
+    );
+  } else {
+    console.log(GREEN, "[ESP32] PRESSURE DATA EMITTED");
+    sendWSObject(
+      WSMessageType.pressure_data,
+      parseESP32PressureData(message),
+      ws
+    );
+  }
+}
+
+/**
+ * Sends a WSObject to all clients
+ * @param message
+ */
 function sendAll(message: string) {
   for (var i = 0; i < CLIENTS.length; i++) {
     CLIENTS[i].send("> SERVER: " + message);
@@ -126,6 +140,13 @@ function sendAll(message: string) {
   }
 }
 
+/**
+ * Emits to all websocket clients except senderWS
+ * @param type
+ * @param data
+ * @param senderWS
+ * @param rawText
+ */
 function broadcast(
   type: WSMessageType,
   data: string,
@@ -141,6 +162,12 @@ function broadcast(
   console.log("Broadcast: ", data.toString());
 }
 
+/**
+ * Sends a JSON formatted WS Object to the specified WS client
+ * @param type
+ * @param data
+ * @param ws
+ */
 function sendWSObject(
   type: WSMessageType,
   data: string,
@@ -149,6 +176,11 @@ function sendWSObject(
   ws.send(constructWSObject(type, data));
 }
 
+/**
+ * Checks whether or not a given string is JSON
+ * @param str
+ * @returns
+ */
 function isJson(str: string): boolean {
   try {
     JSON.parse(str);
