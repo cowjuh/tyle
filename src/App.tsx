22ab610fea0e266e8616e4eb8cbfc2c1e5748a47
Jenny Zhang <jenzhang530@gmail.com>
2022-04-04 22:51:54 -0700

    Use abstracted context providers

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,17 @@ import NotFound from "./components/pages/NotFound";
 import DataMode from "./components/pages/Playground/DataMode";
 import DrawingMode from "./components/pages/Playground/DrawMode";
 import ProgramMode from "./components/pages/Playground/ProgramMode";
-import {
-  GlobalContext,
-  GlobalContextProps,
-} from "./components/context/globalContext";
-import { useEffect, useState } from "react";
-import {
-  LocalStorageKeys,
-  PlaygroundMode,
-  TileGridObject,
-  TileGridPressure,
-} from "./components/types/types";
-import { useRouteLocation } from "./components/hooks/useRouteLocation";
-import { WebSocketProvider } from "./components/context/webSocketContext";
-import { useWebSocket } from "./components/hooks/useWebSocket";
+import { GlobalContextProvider } from "./components/context/globalContext";
+import { useState } from "react";
+import { TileGridPressure } from "./components/types/types";
 import {
   PressureDataContext,
   PressureDataProvider,
 } from "./components/context/pressureDataContext";
 import WebSocketHLC from "./components/WebSocketHLC";
-import { getLocalStorageItem } from "./utils/helpers";
+import { DrawModeContextProvider } from "./components/context/drawModeContext";
 
 function App() {
-  const [globalTileGridObject, setGlobalTileGridObject] =
-    useState<TileGridObject>(
-      getLocalStorageItem(LocalStorageKeys.GLOBAL_TILE_GRID_LS_OBJ || [])
-    );
-  const globalContextValue: GlobalContextProps = {
-    globalTileGridObject: globalTileGridObject,
-    setGlobalTileGridObject: setGlobalTileGridObject,
-  };
-
   const [pressureDataObject, setPressureDataObject] =
     useState<TileGridPressure>([]);
 
@@ -47,28 +27,30 @@ function App() {
 
   return (
     <div className="App">
-      <GlobalContext.Provider value={globalContextValue}>
-        <PressureDataContext.Provider value={pressureDataContextValue}>
-          <WebSocketHLC>
-            <Router>
-              <Routes>
-                <Route path="/" element={<WelcomePage />}></Route>
-                <Route path="/pairing" element={<PairingPage />}></Route>
-                <Route path="/playground/data" element={<DataMode />}></Route>
-                <Route
-                  path="/playground/draw"
-                  element={<DrawingMode />}
-                ></Route>
-                <Route
-                  path="/playground/program/*"
-                  element={<ProgramMode />}
-                ></Route>
-                <Route path="*" element={<NotFound />}></Route>
-              </Routes>
-            </Router>
-          </WebSocketHLC>
-        </PressureDataContext.Provider>
-      </GlobalContext.Provider>
+      <GlobalContextProvider>
+        <DrawModeContextProvider>
+          <PressureDataProvider>
+            <WebSocketHLC>
+              <Router>
+                <Routes>
+                  <Route path="/" element={<WelcomePage />}></Route>
+                  <Route path="/pairing" element={<PairingPage />}></Route>
+                  <Route path="/playground/data" element={<DataMode />}></Route>
+                  <Route
+                    path="/playground/draw"
+                    element={<DrawingMode />}
+                  ></Route>
+                  <Route
+                    path="/playground/program/*"
+                    element={<ProgramMode />}
+                  ></Route>
+                  <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+              </Router>
+            </WebSocketHLC>
+          </PressureDataProvider>
+        </DrawModeContextProvider>
+      </GlobalContextProvider>
     </div>
   );
 }
