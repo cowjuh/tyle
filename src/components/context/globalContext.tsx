c80d944e915707562e897fe91e9cd8a657a7bd9f
Jenny Zhang <jenzhang530@gmail.com>
2022-04-04 22:51:07 -0700

    Abstract GlobalContextProvider

--- src/components/context/globalContext.tsx
+++ src/components/context/globalContext.tsx
@@ -1,15 +1,34 @@
-import { createContext } from "react";
-import { DStreamTileGridObject, TileGridObject } from "../types/types";
+import { createContext, ReactChild, useState } from "react";
+import { getLocalStorageItem } from "../../utils/helpers";
+import { LocalStorageKeys, TileGridObject } from "../types/types";
 
 //TODO: Change globalTileGrid to not optional
 export interface GlobalContextProps {
-  globalTileGrid?: DStreamTileGridObject;
   globalTileGridObject: TileGridObject;
   setGlobalTileGridObject: (obj: TileGridObject) => void;
 }
 
 export const GlobalContext = createContext<GlobalContextProps>({
-  globalTileGrid: [],
   globalTileGridObject: [],
   setGlobalTileGridObject: () => {},
 });
+
+interface IGlobalContextProvider {
+  children: ReactChild;
+}
+
+export const GlobalContextProvider = (props: IGlobalContextProvider) => {
+  const [globalTileGridObject, setGlobalTileGridObject] =
+    useState<TileGridObject>(
+      getLocalStorageItem(LocalStorageKeys.GLOBAL_TILE_GRID_LS_OBJ || [])
+    );
+  const globalContextValue: GlobalContextProps = {
+    globalTileGridObject: globalTileGridObject,
+    setGlobalTileGridObject: setGlobalTileGridObject,
+  };
+  return (
+    <GlobalContext.Provider value={globalContextValue}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
